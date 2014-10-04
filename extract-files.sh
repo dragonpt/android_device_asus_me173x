92b673721ad7cca1f1cea9fdfea6d3ab98f5f3af
Kashif Minhaj <dailypctips@gmail.com>
2014-10-04 14:22:37 -0400

    Clean up

--- extract-files.sh
+++ extract-files.sh
@@ -12,14 +12,14 @@ DIR=`dirname $FILE`
 mkdir -p $BASE/$DIR
     fi
 
-cp /home/kashif/android/system/$FILE $BASE/$FILE
+cp /home/kashif/system/$FILE $BASE/$FILE
 
 #adb pull /system/$FILE $BASE/$FILE
 done
 
 # some extra stuff
-#cp /home/kashif/android/system/lib/hw/audio_policy.default.so $BASE/lib/hw/audio_policy.mt6589.so
-cp /home/kashif/android/system/lib/libaudio.primary.default.so $BASE/lib/hw/audio.primary.mt6589.so
+cp /home/kashif/system/lib/hw/audio_policy.default.so $BASE/lib/hw/audio_policy.mt6589.so
+cp /home/kashif/system/lib/libaudio.primary.default.so $BASE/lib/hw/audio.primary.mt6589.so
 
 #adb pull /system/lib/hw/audio_policy.default.so $BASE/lib/hw/audio_policy.mt6589.so
 #adb pull /system/lib/libaudio.primary.default.so $BASE/lib/hw/audio.primary.mt6589.so
