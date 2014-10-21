75a2f5c421ed22b03c01c8d7145bb79bfc2c9b7b
Kashif Minhaj <dailypctips@gmail.com>
2014-10-21 01:57:01 -0400

    a116: Add more permissions for property services

--- include/device_perms.h
+++ include/device_perms.h
@@ -18,7 +18,33 @@
 #define DEVICE_PERMS_H
 
 #define PROPERTY_PERMS_APPEND \
-    { "nvram_init",      AID_NVRAM,   0 },
+    { "nvram_init",      AID_NVRAM,   0 }, \
+    { "persist.ril.",     AID_RADIO,    0 }, \
+    { "persist.ril.cfu.querytype", AID_APP,   0 }, \
+    { "mux.",             AID_RADIO,    0 }, \
+    { "mux.",             AID_SYSTEM,   0 }, \
+    { "mtk_telephony_mode_slot1",             AID_SYSTEM,   0 }, \
+    { "mtk_telephony_mode_slot2",             AID_SYSTEM,   0 }, \
+    { "net.",             AID_DHCP,   0 }, \
+    { "gps.",             AID_GPS,     AID_SYSTEM }, \
+    { "persist.af.",      AID_MEDIA,   0 }, \
+    { "af.",              AID_MEDIA,   0 }, \
+    { "a2dp.",            AID_MEDIA,   0 }, \
+    { "streamin.",        AID_MEDIA,   0 }, \
+    { "streamout.",       AID_MEDIA,   0 }, \
+    { "bwc.mm.",          AID_MEDIA,   0 }, \
+    { "mediatek.",        AID_RADIO,   0 }, \
+    { "bt.",        AID_BLUETOOTH,    0 }, \
+    { "persist.bt.",      AID_BLUETOOTH,    0 }, \
+    { "persist.mtk.wcn.combo.",        AID_SYSTEM,    0 }, \
+    { "persist.mtklog.",  AID_SHELL,   0 }, \
+    { "ter.",             AID_RADIO,    0 }, \
+    { "persist.ter.",     AID_RADIO,    0 }, \
+    { "media.wfd",  AID_SYSTEM,   AID_MEDIA }, \
+    { "wfd.",  AID_SYSTEM,   AID_MEDIA }, \
+    { "gps.",             AID_GPS,     AID_SYSTEM }, \
+    { "bgw.", 				AID_SYSTEM,    0 }, \
+    { "cdma.",        AID_RADIO,    0 },  
 
 #define CONTROL_PERMS_APPEND \
     { "md_minilog_util", AID_RADIO, AID_RADIO}, \
