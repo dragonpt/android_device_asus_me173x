50c5381a53f47d5b4c4ea7dcc3c1cb466d124426
Cesar <cesar.maximo@gmail.com>
2019-10-03 20:51:45 +0100

    Correct BT name

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,7 +1,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Huawei G700"
+#define BTM_DEF_LOCAL_NAME   "Asus ME173X"
 #define BTA_AV_MAX_A2DP_MTU  1788
 
 #endif
