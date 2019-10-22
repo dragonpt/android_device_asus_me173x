f211bb6398154b4b6da86783d2a114be5fb4ee6d
Cesar <cesar.maximo@gmail.com>
2019-10-22 17:44:11 +0100

    device: media: Move this to configs folder

--- device_me173x.mk
+++ device_me173x.mk
@@ -69,8 +69,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
         frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
         frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video_le.xml \
-	$(LOCAL_PATH)/media_codecs.xml:system/etc/media_codecs.xml \
-	$(LOCAL_PATH)/media_profiles.xml:system/etc/media_profiles.xml
+	$(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+	$(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
 
 # WiFi
 PRODUCT_PACKAGES += \
