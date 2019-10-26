8b2a80a1d070787d1ac5097824d13ec00b61fe41
Cesar <cesar.maximo@gmail.com>
2019-10-26 13:20:20 +0100

    device: pvrsrvctl: Fix up & enable service
    And finnaly i can rm the prebuilt vendor s* binary

--- pvrsrvctl/Android.mk
+++ pvrsrvctl/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := pvrsrvctl.c
 LOCAL_LDFLAGS := -L vendor/asus/me173x/proprietary/vendor/lib
 LOCAL_LDLIBS := -lsrv_init -lsrv_um
-LOCAL_MODULE_PATH := $(TARGET_OUT)/bin/
+LOCAL_MODULE_PATH := $(TARGET_OUT)/vendor/bin/
 LOCAL_MODULE := pvrsrvctl
 LOCAL_MODULE_TAGS := optional
 
