eaef9e4fb3e3fadd5ab604cdf877c15824c1d416
Cesar <cesar.maximo@gmail.com>
2019-10-28 19:03:31 +0000

    charger: clean Healthd

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,17 +80,11 @@ BOARD_BLUETOOTH_DOES_NOT_USE_RFKILL := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/asus/me173x/bluetooth
 
 # Healthd & Charging
-HEALTHD_ENABLE_TRICOLOR_LED := true
-RED_LED_PATH := /sys/class/leds/red/brightness
-GREEN_LED_PATH := /sys/class/leds/green/brightness
-BLUE_LED_PATH := /sys/class/leds/blue/brightness
-# Next lines, fix charging-mod in power off. It needs to modify the init.cpp, too.
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/BOOT/BOOT/boot/boot_mode
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_SHOW_PERCENTAGE := true
 BOARD_HAL_STATIC_LIBRARIES := libhealthd.mtk
-BOARD_HEALTHD_CUSTOM_CHARGER_RES := $(DEVICE_FOLDER)/charger/images
 
 # power
 TARGET_POWERHAL_VARIANT := cm
