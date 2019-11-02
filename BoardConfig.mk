0e8efb4376ddd580c836d9e6267adfe00166f7b3
Cesar <cesar.maximo@gmail.com>
2019-11-02 22:25:32 +0000

    board: Set shader cache configuration options Set the maximum shader cache size and key size that will be used by Open GL

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -105,6 +105,16 @@ BOARD_EGL_CFG := device/asus/me173x/configs/egl.cfg
 USE_OPENGL_RENDERER := true
 BOARD_EGL_WORKAROUND_BUG_10194508 := true
 
+# Shader cache config options
+# Maximum size of the  GLES Shaders that can be cached for reuse.
+# Increase the size if shaders of size greater than 12KB are used.
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+
+# Maximum GLES shader cache size for each app to store the compiled shader
+# binaries. Decrease the size if RAM or Flash Storage size is a limitation
+# of the device.
+MAX_EGL_CACHE_SIZE := 2048*1024
+
 # kernel
 TARGET_PREBUILT_KERNEL := $(DEVICE_FOLDER)/prebuilt/kernel
 
