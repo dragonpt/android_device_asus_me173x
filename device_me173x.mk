6c60dfed451172918ae2799a12890a5afb82286b
Cesar <cesar.maximo@gmail.com>
2020-04-04 16:57:03 +0100

    OMX: Improvements
    * Remove Mediatek encoders & decoders, they are all broken or with very bad performance
    * Fix derp cp the wrong google video xml
    * Don't include ffmpeg.xml on media codecs, all we need from ffmpeg is already included here

--- device_me173x.mk
+++ device_me173x.mk
@@ -71,7 +71,7 @@ PRODUCT_COPY_FILES += \
 # Media 
 PRODUCT_COPY_FILES += \
         frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-        frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video_le.xml \
+        frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml \
 	$(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
 	$(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
 
