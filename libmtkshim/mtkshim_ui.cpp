81011fbbe976696f6eee75ab53a09b4a0a9212bd
Cesar <cesar.maximo@gmail.com>
2021-09-01 23:03:23 +0100

    device: ui shim: Clean unused symbols

--- libmtkshim/mtkshim_ui.cpp
+++ libmtkshim/mtkshim_ui.cpp
@@ -13,10 +13,4 @@ extern "C" {
     void _ZN7android19GraphicBufferMapper4lockEPK13native_handleiRKNS_4RectEPPv(buffer_handle_t handle, int usage, const android::Rect& bounds, void** vaddr) {
         _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPv(handle, static_cast<uint32_t>(usage), bounds, vaddr); //superdragonpt: for libcam.client, libMtkOmxVenc, etc
     }
-
-    void _ZN7android5Fence4waitEi(int);
-
-    void _ZN7android5Fence4waitEj(unsigned int timeout) {
-        _ZN7android5Fence4waitEi(static_cast<int>(timeout));
-    }
 }
